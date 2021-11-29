125beb1f0f393c7078902037995ab2d48ef34804
liaokongVFX <568250549@qq.com>
2021-11-29 23:32:45 +0800

    项目初始化

---
+++ pages/index/index.vue
@@ -0,0 +1,26 @@
+<template>
+	<view>
+		<view class="animated" hover-class="fadeIn" style="border: 1px solid #007AFF;padding: 20rpx;">
+			点击效果
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+</style>
