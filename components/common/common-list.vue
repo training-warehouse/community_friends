97aefb57e608c3f666c3675cd098cc5bfdc445d2
liaokongVFX <568250549@qq.com>
2021-12-06 23:28:34 +0800

    完成列表组件

--- components/common/common-list.vue
+++ components/common/common-list.vue
@@ -3,7 +3,8 @@
 		<!-- 头像 -->
 		<view class="flex align-center justify-between">
 			<view class="flex align-center">
-				<image :src="item.userpic" class="rounded-circle mr-2" style="width: 65rpx;height: 65rpx;" lazy-load>
+				<image :src="item.userpic" class="rounded-circle mr-2" style="width: 65rpx;height: 65rpx;" lazy-load
+					@click="openSpace">
 				</image>
 				<view>
 					<view class="font" style="line-height: 1.5;">{{item.username}}</view>
@@ -11,36 +12,42 @@
 				</view>
 			</view>
 			<view>
-				<view class="flex align-center justify-center rounded bg-main text-white"
-					style="width: 90rpx;height: 50rpx;">
+				<view v-if="!item.isFollow"
+					class="flex align-center justify-center rounded bg-main text-white animated faster"
+					hover-class="jello" style="width: 90rpx;height: 50rpx;" @click="follow">
 					关注</view>
 			</view>
 		</view>
 
 		<!-- 标题 -->
-		<view class="my-1 font-md">
+		<view class="my-1 font-md" @click="openDetail">
 			{{item.title}}
 		</view>
 		<!-- 图片 -->
-		<image :src="item.titlepic" class="rounded w-100" style="height: 350rpx;">
+		<image v-if="item.titlepic" :src="item.titlepic" class="rounded w-100" style="height: 350rpx;"
+			@click="openDetail">
 		</image>
 		<!-- 图标按钮 -->
 		<view class="flex align-center">
-			<view class="flex align-center justify-center flex-1">
+			<view class="flex align-center justify-center flex-1 animated faster" hover-class="jello text-main"
+				:class="item.support.type === 'support'? 'support-active':''" @click="doSupport('support')">
 				<text class="iconfont icon-dianzan2 mr-2"></text>
-				<text>{{item.support.support_count}}</text>
+				<text>{{item.support.support_count === 0 ? '顶':item.support.support_count}}</text>
 			</view>
-			<view class="flex align-center justify-center flex-1">
+			<view class="flex align-center justify-center flex-1 animated faster" hover-class="jello text-main"
+				:class="item.support.type === 'unsupport'? 'support-active':''" @click="doSupport('unsupport')">
 				<text class="iconfont icon-cai mr-2"></text>
-				<text>{{item.support.unsupport_count}}</text>
+				<text>{{item.support.unsupport_count === 0 ? '踩':item.support.unsupport_count}}</text>
 			</view>
-			<view class="flex align-center justify-center flex-1">
+			<view class="flex align-center justify-center flex-1 animated faster" hover-class="jello text-main"
+				@click="openDetail">
 				<text class="iconfont icon-pinglun2 mr-2"></text>
-				<text>{{item.comment_count}}</text>
+				<text>{{item.comment_count === 0 ? '评论':item.comment_count}}</text>
 			</view>
-			<view class="flex align-center justify-center flex-1">
+			<view class="flex align-center justify-center flex-1 animated faster" hover-class="jello text-main"
+				@click="openDetail">
 				<text class="iconfont icon-fenxiang mr-2"></text>
-				<text>{{item.share_num}}</text>
+				<text>{{item.share_num === 0 ? '分享':item.share_num}}</text>
 			</view>
 		</view>
 	</view>
@@ -51,9 +58,29 @@
 		props: {
 			item: Object,
 			index: Number
+		},
+		methods: {
+			openSpace() {
+				console.log('openSpace')
+			},
+			follow() {
+				this.$emit('follow', this.index)
+			},
+			openDetail() {
+				console.log('openDetail')
+			},
+			doSupport(type) {
+				this.$emit('doSupport', {
+					type,
+					index: this.index
+				})
+			}
 		}
 	}
 </script>
 
 <style>
+	.support-active {
+		color: #FF4A6A;
+	}
 </style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,7 +1,7 @@
 <template>
 	<view>
 		<block v-for="(item,index) in list" :key="index">
-			<common-list :item="item" :index="index"></common-list>
+			<common-list :item="item" :index="index" @follow="follow" @doSupport=doSupport></common-list>
 			<divider></divider>
 		</block>
 	</view>
@@ -9,9 +9,9 @@
 
 <script>
 	import commonList from '@/components/common/common-list.vue'
-	
+
 	export default {
-		components:{
+		components: {
 			commonList
 		},
 		data() {
@@ -30,6 +30,34 @@
 					},
 					comment_count: 2,
 					share_num: 1
+				}, {
+					username: "昵称",
+					userpic: "/static/default.jpg",
+					newstime: "2021-12-02 下午23:50",
+					isFollow: false,
+					title: "我是标题",
+					titlepic: "",
+					support: {
+						type: "unsupport",
+						support_count: 1,
+						unsupport_count: 2
+					},
+					comment_count: 0,
+					share_num: 1
+				}, {
+					username: "昵称",
+					userpic: "/static/default.jpg",
+					newstime: "2021-12-02 下午23:50",
+					isFollow: false,
+					title: "我是标题",
+					titlepic: "",
+					support: {
+						type: "",
+						support_count: 1,
+						unsupport_count: 0
+					},
+					comment_count: 2,
+					share_num: 0
 				}]
 			}
 		},
@@ -37,7 +65,31 @@
 
 		},
 		methods: {
+			follow(index) {
+				this.list[index].isFollow = true
+				uni.showToast({
+					title: '关注成功'
+				})
+			},
+			doSupport(data) {
+				let item = this.list[data.index]
+				let msg = data.type === 'support' ? '顶' : '踩'
 
+				if (item.support.type === '') {
+					item.support[data.type + '_count']++
+				} else if (item.support.type === 'support' && data.type === 'unsupport') {
+					item.support.support_count--
+					item.support.unsupport_count++
+				} else if (item.support.type === 'unsupport' && data.type === 'support') {
+					item.support.unsupport_count--
+					item.support.support_count++
+				}
+
+				item.support.type = data.type
+				uni.showToast({
+					'title': msg + '成功'
+				})
+			}
 		}
 	}
 </script>
